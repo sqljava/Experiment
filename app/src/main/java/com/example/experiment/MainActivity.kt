9f0e7440ac90f8a526624bbe092af5201290386e
sqljava <qewr>
2023-03-31 09:55:03 +0500

    bir

---
+++ app/src/main/java/com/example/experiment/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.experiment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.experiment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.main,HomeFragment()).commit()
+
+    }
+}
